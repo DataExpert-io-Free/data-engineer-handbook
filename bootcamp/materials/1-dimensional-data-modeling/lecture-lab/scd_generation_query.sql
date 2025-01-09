8220bac6761b3f33fe180c64a63da1e838592095
gergdt <92580857+gergdt@users.noreply.github.com>
2025-01-09 01:18:43 +0100

    Added is_active information
    To aggregate information to a player historically I added the is_active column so the information is kept to follow the Lab in the next steps.

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/scd_generation_query.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/scd_generation_query.sql
@@ -2,9 +2,14 @@ WITH streak_started AS (
     SELECT player_name,
            current_season,
            scoring_class,
+		   is_active,
            LAG(scoring_class, 1) OVER
                (PARTITION BY player_name ORDER BY current_season) <> scoring_class
-               OR LAG(scoring_class, 1) OVER
+               OR LAG(is_active, 1) OVER
+               (PARTITION BY player_name ORDER BY current_season) <> is_active
+			   OR LAG(is_active, 1) OVER
+               (PARTITION BY player_name ORDER BY current_season) IS NULL
+			   OR LAG(scoring_class, 1) OVER
                (PARTITION BY player_name ORDER BY current_season) IS NULL
                AS did_change
     FROM players
@@ -14,6 +19,7 @@ WITH streak_started AS (
             player_name,
                 scoring_class,
                 current_season,
+				is_active,
             SUM(CASE WHEN did_change THEN 1 ELSE 0 END)
                 OVER (PARTITION BY player_name ORDER BY current_season) as streak_identifier
          FROM streak_started
@@ -22,12 +28,13 @@ WITH streak_started AS (
          SELECT
             player_name,
             scoring_class,
-            streak_identifier,
+			is_active,
             MIN(current_season) AS start_date,
-            MAX(current_season) AS end_date
+            MAX(current_season) AS end_date,
+			2021 as current_season
          FROM streak_identified
-         GROUP BY 1,2,3
+         GROUP BY player_name, streak_identifier, is_active, scoring_class
      )
-
-     SELECT player_name, scoring_class, start_date, end_date
+     SELECT player_name, scoring_class, is_active, start_date, end_date, current_season
      FROM aggregated
+	 ORDER BY player_name;
